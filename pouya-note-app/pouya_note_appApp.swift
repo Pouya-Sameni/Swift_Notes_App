ce236f4d8b256a270048ede8ef4900798941cf72
Pouya Sameni <39872025+Pouya-Sameni@users.noreply.github.com>
2023-09-27 20:14:37 -0400

    Initial Commit

---
+++ pouya-note-app/pouya_note_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pouya_note_appApp.swift
+//  pouya-note-app
+//
+//  Created by Pouya Sameni on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct pouya_note_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
